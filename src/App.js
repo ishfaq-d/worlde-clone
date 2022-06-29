6f2db05561a4a4583e060f27fe5d9bc7cde1277e
ishfaq-d <dareshfaq1999@gmail.com>
2022-06-29 15:24:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Lines from './components/Lines';
+import getWords from './data/words'
 
 function App() {
+
+  const [solution , setSolution] = useState('');
+
+  const [guesses, setGuesses] = useState(Array(6).fill(null));
+
+  const [currentGuess, setCurrentGuess] = useState('');
+
+  const [isGameOver, setIsGameOver] = useState(false)
+
+  useEffect(()=> {
+
+    const handleType = (e) => {
+
+
+      if(isGameOver) return;
+
+
+
+
+      if(e.key === 'Enter'){
+        const isCorrect = solution === currentGuess;
+
+        if(currentGuess.length !==5){
+          return;
+        }
+
+        const newGuesses = [...guesses];
+        newGuesses[guesses.findIndex(val => val === null)] = currentGuess;
+        setGuesses(newGuesses);
+        setCurrentGuess('')
+
+        if(isCorrect) setIsGameOver(true) 
+      }
+
+     
+
+      if(e.key === 'Backspace') {
+        setCurrentGuess(currentGuess.slice(0,-1));
+        return;
+      }
+
+
+      if(currentGuess.length >= 5) {
+        return;
+      }
+
+      const isLetter = e.key.match(/^[a-zA-Z]{1}$/) != null;
+      if(isLetter){
+        setCurrentGuess(oldGuess => oldGuess + (e.key).toLowerCase())
+      }
+
+
+    }
+
+    window.addEventListener('keydown' , handleType);
+
+    return () => window.removeEventListener('keydown',handleType)
+
+  }, [currentGuess,guesses, isGameOver, solution])
+
+
+  useEffect(()=> {
+    const words = getWords();
+    const randomWord = words[Math.floor(Math.random()*words.length)];
+    setSolution(randomWord.toLowerCase());
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      {
+        guesses.map((guess, i) => {
+          const isCurrentGuess =  i === guesses.findIndex(val => val === null)
+          return <Lines 
+          key={i} 
+          guess={isCurrentGuess ? currentGuess : guess ?? ''}
+          isFinal = {!currentGuess && guess !== null}
+          solution={solution}
+          />
+        })
+      }
     </div>
   );
 }
---
+++ src/components/Lines.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const WORD_LENGTH = 5;
+
+function Lines({guess, isFinal , solution}) {
+
+    const tiles  =  [];
+
+    for( let i = 0; i < WORD_LENGTH; i++){
+        const char = guess[i];
+
+
+        let className = 'tiles'
+
+        if(isFinal) {
+            if(char === solution[i]){
+                className += ' correct'
+            }else if(solution.includes(char)){
+                className += " close"
+            }else {
+                className += ' incorrect'
+            }
+        }
+
+            tiles.push(<div className={className}>{char}</div>)
+    }
+
+
+  return (
+    <div className='lines'>
+    {tiles}
+    </div>
+  )
+}
+
+export default Lines
